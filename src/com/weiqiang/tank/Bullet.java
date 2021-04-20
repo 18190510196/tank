cd3d540bcb4e62cc8af27fd6da421e91a5e08e07
18190510196@163.com <18190510196@163.com>
2021-04-20 20:54:01 +0800

    加载子弹图片

--- src/com/weiqiang/tank/Bullet.java
+++ src/com/weiqiang/tank/Bullet.java
@@ -26,10 +26,22 @@ public class Bullet {
         if (!live) {
             tf.bullets.remove(this);
         }
-        Color c = g.getColor();
-        g.setColor(Color.red);
-        g.fillOval(x, y, WIDTH, HEIGHT);
-        g.setColor(c);
+        switch (dir) {
+            case UP:
+                g.drawImage(ResourceMgr.bulletU, x, y, null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.bulletD, x, y, null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.bulletR, x, y, null);
+                break;
+            case LEFT:
+                g.drawImage(ResourceMgr.bulletL, x, y, null);
+                break;
+            default:
+                break;
+        }
         move();
     }
 
--- src/com/weiqiang/tank/ResourceMgr.java
+++ src/com/weiqiang/tank/ResourceMgr.java
@@ -10,13 +10,20 @@ import java.awt.image.BufferedImage;
  **/
 public class ResourceMgr {
     public static BufferedImage tankL, tankD, tankU, tankR;
+    public static BufferedImage bulletL, bulletD, bulletU, bulletR;
 
     static {
         try {
+            //坦克图片加载
             tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
             tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
             tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
             tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
+            //子弹图片加载
+            bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
+            bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
+            bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+            bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
 
         } catch (Exception e) {
             e.printStackTrace();
